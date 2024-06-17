d4063905b439e15d5fef6f5d65f70566dde6aadd
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-06-17 20:31:05 -0300

    a

--- src/galeria.cpp
+++ src/galeria.cpp
@@ -12,25 +12,52 @@ struct rep_galeria{
 };
 
 void agregarPiezaTGaleria(TGaleria galeria, TPieza pieza){
+    insertarPiezaColeccionPiezas(galeria->coleccion,pieza);
 }
 
 void agregarExposicionTGaleria(TGaleria galeria, TExposicion expo){
+    if (compararTFechas(galeria->fecha,fechaInicioTExposicion(expo)) < 0){
+        agregarExposicionTListaExposiciones(galeria->futuras, expo);
+    } else {
+        if (compararTFechas(galeria->fecha, fechaInicioTExposicion(expo)) >= 0){
+            if (compararTFechas(galeria->fecha, fechaFinTExposicion(expo)) > 0){
+                agregarExposicionTListaExposiciones(galeria->finalizadas, expo);
+            } else {
+                agregarExposicionTListaExposiciones(galeria->activas,expo);
+            }
+        }
+    }
 }
 
 void agregarPiezaAExposicionTGaleria(TGaleria galeria, int idPieza, int idExpo){
+    TPieza pieza = obtenerPiezaColeccionPiezas(galeria->coleccion,idPieza);
+    TExposicion destino = NULL;
+    if (perteneceExposicionTListaExposiciones(galeria->activas, idExpo)){
+        destino = obtenerExposicionTListaExposiciones(galeria->activas,idExpo);
+    } else if (perteneceExposicionTListaExposiciones(galeria->futuras, idExpo)){
+        destino = obtenerExposicionTListaExposiciones(galeria->futuras,idExpo);
+    } else {
+        destino = obtenerExposicionTListaExposiciones(galeria->finalizadas,idExpo);
+    }
+    agregarATExposicion(destino, pieza);
 }
 
 void imprimirExposicionesFinalizadasTGaleria(TGaleria galeria){
+    imprimirTListaExposiciones(galeria->finalizadas);
 }
 
 void imprimirExposicionesActivasTGaleria(TGaleria galeria){
+    imprimirTListaExposiciones(galeria->activas);
 }
 
 void imprimirExposicionesFuturasTGaleria(TGaleria galeria){
+    imprimirTListaExposiciones(galeria->futuras);
 }
 
 bool esCompatibleExposicionTGaleria(TGaleria galeria, TExposicion expo){
-    return false;
+    return (esCompatibleTListaExposiciones(galeria->activas,expo) &&
+    esCompatibleTListaExposiciones(galeria->futuras, expo) &&
+    esCompatibleTListaExposiciones(galeria->finalizadas, expo));
 }
 
 TGaleria crearTGaleria(TFecha fecha){
