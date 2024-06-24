56811660a60f3cb276e95719a0015eb996ccdbc7
pablo4lv <93752198+pablo4lv@users.noreply.github.com>
2024-06-24 14:53:26 -0300

    a

--- src/galeria.cpp
+++ src/galeria.cpp
@@ -90,7 +90,7 @@ void avanzarAFechaTGaleria(TGaleria galeria, TFecha fecha){
 
     agregarVisitaDiaTHashVisitaDia(galeria->hash, galeria->visitaDia);
     //
-    liberarTVisitaDia(galeria->visitaDia);
+    // liberarTVisitaDia(galeria->visitaDia);
 
     galeria->visitaDia = crearTVisitaDia(copiarTFecha(fecha), MAX_GRUPOS_VISITA_DIA);
 
